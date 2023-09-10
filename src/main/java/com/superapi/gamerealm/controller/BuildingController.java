d80e05ea42eedba93a15bbea6f140f4743361e1c
Esparguetes <diogobarrosmartins@hotmail.com>
2023-09-10 05:02:34 +0100

    purge dirty logic

--- src/main/java/com/superapi/gamerealm/controller/BuildingController.java
+++ src/main/java/com/superapi/gamerealm/controller/BuildingController.java
@@ -2,10 +2,7 @@ package com.superapi.gamerealm.controller;
 
 import com.superapi.gamerealm.dto.building.NonResourceBuildingDTO;
 import com.superapi.gamerealm.dto.building.ResourceBuildingDTO;
-import com.superapi.gamerealm.dto.troops.BuildingTroopInfoDTO;
-import com.superapi.gamerealm.model.buildings.Building;
 import com.superapi.gamerealm.service.BuildingService;
-import com.superapi.gamerealm.service.TroopTrainingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +15,8 @@ import java.util.List;
 @RequestMapping("buildings/{villageId}/")
 public class BuildingController {
     private final BuildingService buildingService;
-    private final TroopTrainingService troopTrainingService;
-    public BuildingController(BuildingService buildingService, TroopTrainingService troopTrainingService) {
+    public BuildingController(BuildingService buildingService) {
         this.buildingService = buildingService;
-        this.troopTrainingService =  troopTrainingService;
     }
 
     @GetMapping("/resource")
@@ -35,26 +30,7 @@ public class BuildingController {
         List<NonResourceBuildingDTO> nonResourceBuildings = buildingService.getAllNonResourceBuildingsInVillage(villageId);
         return ResponseEntity.ok(nonResourceBuildings);
     }
-    @GetMapping("/{buildingId}/buildable-troops")
-    public ResponseEntity<BuildingTroopInfoDTO> getBuildableTroopsForBuilding(
-            @PathVariable Long buildingId
-    ) {
-        // Fetch the building based on villageId and buildingId
-        Building building = buildingService.findById(buildingId);
 
-        if (building == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        // Determine the buildable troops for the building
-        List<String> buildableTroops = buildingService.getBuildableTroopsForBuilding(building);
-
-        BuildingTroopInfoDTO troopInfoDTO = new BuildingTroopInfoDTO();
-        troopInfoDTO.setBuildingType(building.getType().toString());
-        troopInfoDTO.setBuildableTroops(buildableTroops);
-
-        return ResponseEntity.ok(troopInfoDTO);
-    }
 }
 
 
