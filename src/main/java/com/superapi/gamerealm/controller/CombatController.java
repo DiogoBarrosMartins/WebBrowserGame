fafd8b39ea529f433599d63f111c9bbe587c71f5
Esparguetes <diogobarrosmartins@hotmail.com>
2023-09-30 21:11:04 +0100

    Update CombatController.java

--- src/main/java/com/superapi/gamerealm/controller/CombatController.java
+++ src/main/java/com/superapi/gamerealm/controller/CombatController.java
@@ -1,15 +1,9 @@
 package com.superapi.gamerealm.controller;
 
-import com.superapi.gamerealm.model.Village;
-import com.superapi.gamerealm.model.troop.VillageTroops;
 import com.superapi.gamerealm.service.CombatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/combat")
@@ -21,7 +15,7 @@ public class CombatController {
     public CombatController(CombatService combatService) {
         this.combatService = combatService;
     }
-
+/**
     @PostMapping("/attack")
     public String initiateAttack(@RequestBody AttackRequest request) {
         List<VillageTroops> attackingTroops = request.getAttackingTroops();
@@ -31,4 +25,5 @@ public class CombatController {
         combatService.basicAttack(attackingTroops, defendingTroops, defendingVillage);
         return "Attack completed";
     }
-}
+*/
+    }
